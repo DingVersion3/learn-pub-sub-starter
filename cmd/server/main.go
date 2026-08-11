3ac7cc657390ca1e95c58576a506349f1bb51b18
DingVersion3 <jakewilk10@gmail.com>
2026-08-11 10:40:59 -0700

    CH3 L1: Exchanges and Queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,22 +8,41 @@ import (
 	"syscall"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
 	fmt.Println("Starting Peril server...")
 
-	const connectString = "amqp://guest:guest@localhost:5672/"
+	const connString = "amqp://guest:guest@localhost:5672/"
 
-	connect, err := amqp.Dial(connectString)
+	conn, err := amqp.Dial(connString)
 	if err != nil {
 		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
-	defer connect.Close()
+	defer conn.Close()
 
 	fmt.Println("Peril game server connected to RabbitMQ!")
 
-	// block until we get a signal
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Printf("could not publish pause message: %v", err)
+	}
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
