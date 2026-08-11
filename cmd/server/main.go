da941477cd63fb0df8b10d041a686869d3100fcf
DingVersion3 <jakewilk10@gmail.com>
2026-08-11 10:24:14 -0700

    CH2: Message Brokers L3: Connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const connectString = "amqp://guest:guest@localhost:5672/"
+
+	connect, err := amqp.Dial(connectString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer connect.Close()
+
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	// block until we get a signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	fmt.Println("Peril game server shutting down...")
 }
